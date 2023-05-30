ec230c0ea7ab85f1f13cc22b6a8f0f6ab16a5ad1
DorBaram <73188591+DorBaram@users.noreply.github.com>
2023-05-30 15:39:43 +0300

    mlAgents started

---
+++ Assets/Scripts/PlanetExploration/AllowedTiles.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+/**
+ * This component just keeps a list of allowed tiles.
+ * Such a list is used both for pathfinding and for movement.
+ */
+public class AllowedTiles : MonoBehaviour  {
+    [SerializeField] TileBase[] allowedTiles = null;
+
+    public bool Contain(TileBase tile) {
+        return allowedTiles.Contains(tile);
+    }
+
+    public TileBase[] Get() { return allowedTiles;  }
+}
---
+++ Assets/Scripts/PlanetExploration/KeyboardMover.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.InputSystem;
+
+/**
+ * This component allows the player to move by clicking the arrow keys,
+ * but only if the new position is on an allowed tile.
+ */
+public class KeyboardMover : MonoBehaviour
+{
+    [SerializeField] Tilemap tilemap = null;
+    [SerializeField] AllowedTiles allowedTiles = null;
+
+    [SerializeField] InputAction moveAction;
+
+    void OnValidate()
+    {
+        // Provide default bindings for the input actions.
+        // Based on answer by DMGregory: https://gamedev.stackexchange.com/a/205345/18261
+        if (moveAction == null)
+            moveAction = new InputAction(type: InputActionType.Button);
+        if (moveAction.bindings.Count == 0)
+            moveAction.AddCompositeBinding("2DVector")
+                .With("Up", "<Keyboard>/upArrow")
+                .With("Down", "<Keyboard>/downArrow")
+                .With("Left", "<Keyboard>/leftArrow")
+                .With("Right", "<Keyboard>/rightArrow");
+    }
+
+    private void OnEnable()
+    {
+        moveAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        moveAction.Disable();
+    }
+
+    protected Vector3 NewPosition()
+    {
+        if (moveAction.WasPerformedThisFrame())
+        {
+            Vector3 movement = moveAction.ReadValue<Vector2>(); // Implicitly convert Vector2 to Vector3, setting z=0.
+            //Debug.Log("movement: " + movement);
+            return transform.position + movement;
+        }
+        else
+        {
+            return transform.position;
+        }
+    }
+
+    private TileBase TileOnPosition(Vector3 worldPosition)
+    {
+        Vector3Int cellPosition = tilemap.WorldToCell(worldPosition);
+        return tilemap.GetTile(cellPosition);
+    }
+
+    private void GoTile()
+    {
+        Vector3 newPosition = NewPosition();
+        TileBase tileOnNewPosition = TileOnPosition(newPosition);
+        if (allowedTiles.Contain(tileOnNewPosition))
+        {
+            transform.position = newPosition;
+        }
+        else
+        {
+            Debug.Log("You cannot walk on " + tileOnNewPosition + "! ");
+        }
+    }
+
+    void Update()
+    {
+        GoTile();
+        //transform.position = NewPosition();
+    }
+}
