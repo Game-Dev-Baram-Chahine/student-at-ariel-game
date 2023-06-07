a7bfba443f2a55b3152ac18a734b65c3099c2a5b
Ibrahem Chahine <22155702+ibrahimchahine@users.noreply.github.com>
2023-06-07 15:54:56 +0300

    Added more logic for the airplane

--- Assets/Scripts/AirPlane/AirPlaneController.cs
+++ Assets/Scripts/AirPlane/AirPlaneController.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class AirPlaneController : MonoBehaviour
 {
     public float speed;
-    public float acceleration;
     Rigidbody2D rb;
     public float rotation;
     // Start is called before the first frame update
@@ -27,18 +26,15 @@ public class AirPlaneController : MonoBehaviour
         Vector2 velocity = transform.right * (moveX * speed);
         rb.AddForce(velocity);
         float direction = Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.right));
-        if (acceleration > 0)
+        if (direction > 0)
         {
-            if (direction > 0)
-            {
-                rb.rotation += moveY * rotation * (rb.velocity.magnitude / speed);
-            }
-            else
-            {
-                rb.rotation -= moveY * rotation * (rb.velocity.magnitude / speed);
-            }
+            rb.rotation += moveY * rotation * (rb.velocity.magnitude / speed);
         }
-        float thrustForce = Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.down)) * 2.0f;
+        else
+        {
+            rb.rotation -= moveY * rotation * (rb.velocity.magnitude / speed);
+        }
+        float thrustForce = Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.down)) * 15.0f;
         Vector2 relativeForce = Vector2.up * thrustForce;
         rb.AddForce(rb.GetRelativeVector(relativeForce));
 
@@ -46,5 +42,9 @@ public class AirPlaneController : MonoBehaviour
         {
             rb.velocity = rb.velocity.normalized * speed;
         }
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Collision2D: " + other.collider.tag);
     }
 }
