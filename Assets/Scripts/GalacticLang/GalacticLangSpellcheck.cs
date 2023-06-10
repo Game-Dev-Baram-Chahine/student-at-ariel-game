c9c330299f925601f9265bad4fdbbdea14eb2d3c
DorBaram <73188591+DorBaram@users.noreply.github.com>
2023-06-10 16:56:51 +0300

    finished lang game

---
+++ Assets/Scripts/GalacticLang/GalacticLangSpellcheck.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GalacticLangSpellcheck : MonoBehaviour
+{
+    [SerializeField] public string nextLevelTag;  // next level name to go to
+    [SerializeField] public InputField inputField;
+    [SerializeField] public string correctAnswer;
+
+    [SerializeField] public Text messageText;
+    [SerializeField] public string incorrectAnswerMessage = "Incorrect answer!";
+    [SerializeField] public float messageDisplayTime = 2f;
+    private float messageTimer = 0f;
+    private bool displayMessage = false;
+
+    public void CheckAnswer()
+    {
+        string userInput = inputField.text;
+        if (userInput.Equals(correctAnswer))
+        {
+            Debug.Log("Correct answer!");
+            StaticVariables.LoadSceneByName(nextLevelTag);
+        }
+        else
+        {
+            Debug.Log("Incorrect answer!");
+            messageText.text = incorrectAnswerMessage;
+            messageTimer = 0f;
+            displayMessage = true;
+        }
+    }
+
+    void Update()
+    {
+        if (displayMessage)
+        {
+            messageTimer += Time.deltaTime;
+            if (messageTimer >= messageDisplayTime)
+            {
+                displayMessage = false;
+                messageText.text = "";
+            }
+        }
+    }
+}
