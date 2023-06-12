28bfbcca76ffe270db3348555d0d524fe5bc48d9
DorBaram <73188591+DorBaram@users.noreply.github.com>
2023-06-13 00:04:57 +0300

    Update StaticVariables.cs

--- Assets/Scripts/StaticVariables.cs
+++ Assets/Scripts/StaticVariables.cs
@@ -15,7 +15,7 @@ public class StaticVariables : MonoBehaviour
     public static float acadimicScore = 0;
     public static float socialScore = 0;
     public static float passAcademic = 80;
-    public static float passSocial = 40;
+    public static float passSocial = 30;
     public static float maxScore = 100;
     public enum Week { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
     public static Week day = Week.Sunday;
